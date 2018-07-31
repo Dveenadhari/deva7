0b5e10e98083d666cbb93847866168cd9f8e9a80
Dveenadhari <41769824+Dveenadhari@users.noreply.github.com>
2018-07-31 10:41:12 +0530

    Rename veena8.py to veena9.py

---
+++ veena9.py
@@ -0,0 +1,7 @@
+1=input("input letter of alphabet)
+if 1 in ('a','e','i','o','u'):
+  print("%s is a vowel." % 1")
+ elif 1=='y':
+   print("sometimes letter y stand for vowel,sometimes stand for consonant.")
+else:
+print("%s is a consonant." % 1)
